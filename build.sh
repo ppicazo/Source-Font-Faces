a96982ccfd64a1b2619e7efd44e3de8ff1706422
Paul Picazo <ppicazo@gmail.com>
2012-10-02 21:29:17 -0700

    First crack at building the open source fonts for use on the web.

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+fonts=('SourceSansPro' 'SourceCodePro')
+
+# clean existing build artifacts
+rm -rf target/
+mkdir -p target/
+
+thisDir=`pwd`
+
+for f in ${fonts[@]};
+do
+  cd $thisDir/$f
+  ./build.sh
+  cp target/TTF/*.ttf $thisDir/target
+done
+
+cd $thisDir/target
+convertFonts.sh *.ttf
+rm old
+mv stylesheet.css fonts.css
